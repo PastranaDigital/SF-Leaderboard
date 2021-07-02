7dd1325ac5a2fc50c5e1c6bce9bda1881ee22d1c
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-07-02 17:03:29 -0500

    Workout Data is coming in an unreadable format for LWC

--- SP_Vault_Comp/force-app/main/default/lwc/workoutDisplay/workoutDisplay.js
+++ SP_Vault_Comp/force-app/main/default/lwc/workoutDisplay/workoutDisplay.js
@@ -1,7 +1,7 @@
 import { LightningElement, wire } from 'lwc';
 import imageResource from '@salesforce/resourceUrl/SPCompImages';
 import getAllWorkouts from '@salesforce/apex/WorkoutLwcController.getAllWorkouts';
-// import getWorkoutResults from '@salesforce/apex/WorkoutLwcController.getWorkoutResults';
+import getWorkoutsAndResults from '@salesforce/apex/WorkoutLwcController.getWorkoutsAndResults';
 export default class WorkoutDisplay extends LightningElement {
     
     keyLogo = imageResource + '/Images/key_logo.png';
@@ -20,35 +20,28 @@ export default class WorkoutDisplay extends LightningElement {
     // `${rank}. ${athleteName}: ${Score_1st__c} ${First_Label__c} ${Score_2nd__c} ${Second_Label__c} @ ${Weight_Used__c} lbs`
     
     // topScoreString = `${rank}. ${athleteName}: ${Score_1st__c} ${First_Label__c}`;
-    topScoreString = '${rank}. ${athleteName}: ${Score_1st__c} ${First_Label__c}';
+    topScoreString = '{rank.athleteName}: {rank.Score_1st__c} {rank.First_Label__c}';
     
 
-    @wire(getAllWorkouts) workoutList(result) {
+    @wire(getWorkoutsAndResults) workoutList(result) {
+        //? data will be in workoutDisplayObject format
         this.wireWorkoutList = result;
+        // console.log("result.data " + [...result.data]);
+        console.log("result.data " + JSON.stringify(result.data));
         if (result.data) {
             let currentData = [];
-            let rank = 1;
             result.data.forEach((row) => {
-                let rowData = {};
-                rowData.Id = row.Id;
-                rowData.Name = row.Name;
-                rowData.AccordionTitle = `#${row.Order__c} ${row.Name} - ${row.Workout_Date__c}`;
-                rowData.Plain_Description__c = row.Plain_Description__c;
-                rowData.Description__c = row.Description__c;
-                rowData.First_Label__c = row.First_Label__c;
-                rowData.Second_Label__c = row.Second_Label__c;
-                rowData.Order__c = row.Order__c;
-                rowData.Goal__c = row.Goal__c;
-                rowData.Image_File__c = imageResource + '/Images/' + row.Image_File__c;
-                rowData.RX_Weight_Male__c = row.RX_Weight_Male__c;
-                rowData.URL__c = row.URL__c;
-                rowData.Workout_Date__c = row.Workout_Date__c;
-
-                if (row.Second_Label__c) {
-                    this.topScoreString += ` ${Score_2nd__c} ${Second_Label__c}`;
+                let rowData = row;
+                //! can't wipe out the data from the row to rowData
+                
+                rowData.Image_File__c = imageResource + '/Images/' + row.ImageFile;
+                
+                //! needs to be done here
+                if (row.SecondLabel) {
+                    this.topScoreString += ' {rank.Score_2nd__c} {rank.Second_Label__c}';
                 } 
-                if (row.RX_Weight_Male__c) {
-                    this.topScoreString += ` @ ${Weight_Used__c} lbs`;
+                if (row.RXWeightMale) {
+                    this.topScoreString += ' @ {rank.Weight_Used__c} lbs';
                 };
                 rowData.topScoreString = this.topScoreString;
 
@@ -57,11 +50,54 @@ export default class WorkoutDisplay extends LightningElement {
                 // console.log(`topScoreString: ${rowData.topScoreString}`);
             });
             this.data = currentData;
+
+            console.log(currentData[5].topScoreString);
             // console.log(currentData[5].Plain_Description__c);
         } else if (result.error) {
             window.console.log(result.error);
         }
     }
+    // @wire(getAllWorkouts) workoutList(result) {
+    //     this.wireWorkoutList = result;
+    //     if (result.data) {
+    //         let currentData = [];
+    //         let rank = 1;
+    //         result.data.forEach((row) => {
+    //             let rowData = {};
+    //             //! can't wipe out the data from the row to rowData
+    //             rowData.Id = row.Id;
+    //             rowData.Name = row.Name;
+    //             rowData.AccordionTitle = `#${row.Order__c} ${row.Name} - ${row.Workout_Date__c}`;
+    //             rowData.Plain_Description__c = row.Plain_Description__c;
+    //             rowData.Description__c = row.Description__c;
+    //             rowData.First_Label__c = row.First_Label__c;
+    //             rowData.Second_Label__c = row.Second_Label__c;
+    //             rowData.Order__c = row.Order__c;
+    //             rowData.Goal__c = row.Goal__c;
+    //             //!rowData.Image_File__c = imageResource + '/Images/' + row.Image_File__c;
+    //             rowData.RX_Weight_Male__c = row.RX_Weight_Male__c;
+    //             rowData.URL__c = row.URL__c;
+    //             rowData.Workout_Date__c = row.Workout_Date__c;
+
+    //             //! needs to be done here
+    //             if (row.Second_Label__c) {
+    //                 this.topScoreString += ` ${Score_2nd__c} ${Second_Label__c}`;
+    //             } 
+    //             if (row.RX_Weight_Male__c) {
+    //                 this.topScoreString += ` @ ${Weight_Used__c} lbs`;
+    //             };
+    //             rowData.topScoreString = this.topScoreString;
+
+    //             currentData.push(rowData);
+    //             // console.log(`rowData: ${rowData.Workout_Date__c}`);
+    //             // console.log(`topScoreString: ${rowData.topScoreString}`);
+    //         });
+    //         this.data = currentData;
+    //         // console.log(currentData[5].Plain_Description__c);
+    //     } else if (result.error) {
+    //         window.console.log(result.error);
+    //     }
+    // }
 
     activeSectionMessage = '';
     
