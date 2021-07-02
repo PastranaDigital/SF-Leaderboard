dab02491c0d6a1191a522dea531cdb29d60a446c
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-07-02 11:11:01 -0500

    improving Workout page

--- SP_Vault_Comp/force-app/main/default/lwc/workoutDisplay/workoutDisplay.js
+++ SP_Vault_Comp/force-app/main/default/lwc/workoutDisplay/workoutDisplay.js
@@ -1,6 +1,7 @@
 import { LightningElement, wire } from 'lwc';
 import imageResource from '@salesforce/resourceUrl/SPCompImages';
 import getAllWorkouts from '@salesforce/apex/WorkoutLwcController.getAllWorkouts';
+// import getWorkoutResults from '@salesforce/apex/WorkoutLwcController.getWorkoutResults';
 export default class WorkoutDisplay extends LightningElement {
     
     keyLogo = imageResource + '/Images/key_logo.png';
@@ -9,26 +10,29 @@ export default class WorkoutDisplay extends LightningElement {
     //? https://github.com/PastranaDigital/lwc-udemy-course/blob/feature/dev3/force-app/main/default/lwc/accountListViewer/accountListViewer.js
     wireWorkoutList = []; // used for refreshApex
     
-    //? Default Values
-    workoutRecordId = 'a025e0000036GDBAA2'; // workout.Id
-    workoutName = 'Chaos'; // workout.name
-    workoutDescription = '10 min AMRAP'; // workout.Description__c
-    workoutOrder = '1';
-    workoutGoal = 'Finish Round of 9-12’S (90-156 reps)'; // workout.Goal__c
-    workoutImage = '1_Chaos.jpg';
-    workoutRxWeight = '40'; // workout.RX_Weight_Male__c
-    workoutURL = 'https://spmembersonly.com/2021-vault-workouts/2021/01/04/chaos'; // workout.URL__c
-    workoutDate = '7/5/2021'; // workout.Workout_Date__c
+    //? 1. Omar Pastrana: 120 reps
+    // `${rank}. ${athleteName}: ${Score_1st__c} ${First_Label__c}`
+    //? 1. Omar Pastrana: 120 reps @ 40 lbs
+    // `${rank}. ${athleteName}: ${Score_1st__c} ${First_Label__c} @ ${Weight_Used__c} lbs`
+    //? 1. Omar Pastrana: 2 Mins 30 Secs
+    // `${rank}. ${athleteName}: ${Score_1st__c} ${First_Label__c} ${Score_2nd__c} ${Second_Label__c}`
+    //? 1. Omar Pastrana: 2 Rounds 30 reps @ 40 lbs
+    // `${rank}. ${athleteName}: ${Score_1st__c} ${First_Label__c} ${Score_2nd__c} ${Second_Label__c} @ ${Weight_Used__c} lbs`
+    
+    // topScoreString = `${rank}. ${athleteName}: ${Score_1st__c} ${First_Label__c}`;
+    topScoreString = '${rank}. ${athleteName}: ${Score_1st__c} ${First_Label__c}';
+    
 
     @wire(getAllWorkouts) workoutList(result) {
         this.wireWorkoutList = result;
         if (result.data) {
             let currentData = [];
+            let rank = 1;
             result.data.forEach((row) => {
                 let rowData = {};
                 rowData.Id = row.Id;
                 rowData.Name = row.Name;
-                rowData.AccordionTitle = `#${row.Order__c} ${row.Name}`;
+                rowData.AccordionTitle = `#${row.Order__c} ${row.Name} - ${row.Workout_Date__c}`;
                 rowData.Plain_Description__c = row.Plain_Description__c;
                 rowData.Description__c = row.Description__c;
                 rowData.First_Label__c = row.First_Label__c;
@@ -39,10 +43,21 @@ export default class WorkoutDisplay extends LightningElement {
                 rowData.RX_Weight_Male__c = row.RX_Weight_Male__c;
                 rowData.URL__c = row.URL__c;
                 rowData.Workout_Date__c = row.Workout_Date__c;
+
+                if (row.Second_Label__c) {
+                    this.topScoreString += ` ${Score_2nd__c} ${Second_Label__c}`;
+                } 
+                if (row.RX_Weight_Male__c) {
+                    this.topScoreString += ` @ ${Weight_Used__c} lbs`;
+                };
+                rowData.topScoreString = this.topScoreString;
+
                 currentData.push(rowData);
+                // console.log(`rowData: ${rowData.Workout_Date__c}`);
+                // console.log(`topScoreString: ${rowData.topScoreString}`);
             });
             this.data = currentData;
-            // console.log(currentData[0].Workout_Date__c);
+            // console.log(currentData[5].Plain_Description__c);
         } else if (result.error) {
             window.console.log(result.error);
         }
