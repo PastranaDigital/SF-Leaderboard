76c81f3a3b6032d01da7bbff99812bd0ee642414
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-12-29 23:57:06 -0600

    Current Workout is top of page

--- SP_Vault_Comp/force-app/main/default/lwc/twentytwoWorkout/twentytwoWorkout.js
+++ SP_Vault_Comp/force-app/main/default/lwc/twentytwoWorkout/twentytwoWorkout.js
@@ -3,8 +3,35 @@ import { LightningElement, api } from 'lwc';
 export default class TwentytwoWorkout extends LightningElement {
     @api currentWorkout;
 	@api workoutResults;
+	newWorkoutResults = [];
+	buildDataComplete = false;
+
+	setCurrent(incomingArray, value) {
+		//? remove current item 
+		//? move current item to top of list
+		let putFirst = {};
+		let theRest = [];
+	  
+		incomingArray.forEach((element) => {
+			if (element.Name === value.Name) {
+				putFirst = element;
+		  	} else {
+				theRest.push(element);
+		  	}
+		});
+		theRest.unshift(putFirst);
+		this.buildDataComplete = true;
+		return theRest;
+	  };
+	  
+	connectedCallback() {
+		this.buildDataComplete = false;
+	}
 
     renderedCallback() {
 		//? reorder the workoutResults to have the currentWorkout first
+		if(!this.buildDataComplete) {
+			this.newWorkoutResults = this.setCurrent(this.workoutResults, this.currentWorkout);
+		}
     }
 }
